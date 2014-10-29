849787f64885eb4ff4614832d9d08e18b9c5cb1d
Daviduxotto <daviduxotto@gmail.com>
2014-10-29 00:02:42 -0600

    ya activa

--- app/src/main/java/dawareestudio/app_php/MyActivity.java
+++ app/src/main/java/dawareestudio/app_php/MyActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MyActivity extends Activity {
+
+    //declaraciones de objetos
+    private Button btnactivar;
+    private Button btndesactivar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +25,39 @@ public class MyActivity extends Activity {
         StrictMode.setThreadPolicy(policy);
         //codigo para mostrar lo de php
         HttpHandler handler = new HttpHandler();
-        String txt = handler.post("http://192.168.1.50/app.php");
-
+        String txt = handler.post("http://192.168.1.50/alarma/estado.php");
         TextView t = (TextView)findViewById(R.id.TextTexto);
+
+        if(txt.equals("0\n"))
+        {
+            txt="Alarma Desactivada";
+        }
+        else
+        {
+            txt="Alarma activada";
+        }
         t.setText(txt);
         // fin del codigo para mostrar lo de php
+
+        btnactivar = (Button)findViewById(R.id.botonActivar);
+        btndesactivar = (Button)findViewById(R.id.botonDesactivar);
+        //eventos
+        btnactivar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btndesactivar.setEnabled(true);
+                btnactivar.setEnabled(false);
+            }
+        });
+
+        btndesactivar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnactivar.setEnabled(true);
+                btndesactivar.setEnabled(false);
+            }
+        });
+
     }
 
 
